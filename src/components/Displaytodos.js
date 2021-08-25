8ecedceb73a5b1f2f6f2d543b9c4cb23deb1f9ff
Oki Kehinde <olamiposi.oki@gmail.com>
2021-08-25 02:56:20 +0100

    filter tasks functionality

--- src/components/Displaytodos.js
+++ src/components/Displaytodos.js
@@ -1,17 +1,48 @@
-import React, { useContext, useState } from 'react';
-import { CancelButton, TodoCheckBox, ListContainer, ListItem } from './styles';
+import React, { useContext, useState, useEffect } from 'react';
+import {
+	CancelButton,
+	TodoCheckBox,
+	ListContainer,
+	ListItem,
+	TabsWrapper,
+} from './styles';
 import iconcross from '../asserts/images/iconcross.svg';
 import iconcheck from '../asserts/images/iconcheck.svg';
 import TodosContext from '../context/todos/todosContext';
+import Buttons from './Buttons';
 const Displaytodos = () => {
 	const todosContext = useContext(TodosContext);
 
 	const { todos, removeTodo, markAsCompleted } = todosContext;
+	const [todoItem, setTodoItem] = useState(todos);
+
+	console.log(todos);
+	// show the total of uncompeleted task
+	const total = todos.filter((todo) => todo.completed === false);
+	// show both completed and active tasks
+	const showAllTask = () => {
+		const allTask = todos;
+		console.log(allTask);
+		setTodoItem(allTask);
+	};
+	// show only active task
+	const showActive = () => {
+		const active = todoItem.filter((todo) => todo.completed === false);
+		setTodoItem(active);
+		console.log(active);
+	};
+	// show only completed task
+	const completedTask = () => {
+		const completedTasks = todoItem.filter((todo) => todo.completed === true);
+		console.log(completedTasks);
+		setTodoItem(completedTasks);
+	};
+	console.log(total.length);
 
 	return (
 		<>
 			<ListContainer>
-				{todos.map((todo) => (
+				{todoItem.map((todo) => (
 					<ListItem
 						key={todo.id}
 						onClick={() => markAsCompleted(todo.id)}
@@ -29,6 +60,14 @@ const Displaytodos = () => {
 						</span>
 					</ListItem>
 				))}
+				<TabsWrapper>
+					<p>{total.length} items lefts </p>
+					<Buttons
+						showAll={showAllTask}
+						activeTodo={showActive}
+						completedTodo={completedTask}
+					/>
+				</TabsWrapper>
 			</ListContainer>
 		</>
 	);
